ca9e1601e86cdb8c4b96632ac489afb8eebd65e2
piyumi madushani <109897972+Instee@users.noreply.github.com>
2024-11-28 11:03:32 +0530

    commit

--- campus-compass/src/App.js
+++ campus-compass/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import Selection from "./Selection"
+import Contact from "./Contact";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/Selection">Courses</Link></li>
+          <li><Link to="/Contact">Contact</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Selection" element={<Selection />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
-}
+};
+
+
+
 
 export default App;
