2a7a8e4a433a0cb271f25d7aaf33b733b5c9f323
Dilki Nimeshika Silva <139777819+DilkiNimeshika@users.noreply.github.com>
2024-12-17 13:07:32 +0530

    ,login signup (#17)

---
+++ campus-compass/src/components/SignupModal.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Modal.css";
+import { FaFacebookF, FaGoogle } from "react-icons/fa";
+
+
+const SignupModal = ({ onClose, onSwitchToLogin }) => {
+  return (
+    <div className="modal-overlay">
+      <div className="modal">
+        <button className="close-btn" onClick={onClose}>
+          ×
+        </button>
+        <h2>Create Account</h2>
+        
+        <button className="btn social google">
+             <FaGoogle style={{ marginRight: "10px" }} /> Sign in with Google
+        </button>
+        <button className="btn social facebook">
+             <FaFacebookF style={{ marginRight: "10px" }} /> Sign in with Facebook
+        </button>
+        or
+        <hr />
+        <form>
+          
+          <input type="email" placeholder="Enter your email" required />
+          
+          <input type="password" placeholder="Enter your password" required />
+          <button type="submit" className="btn primary">Create Account</button>
+        </form>
+        <p>
+          Already a member?{" "}
+          <a href="#" onClick={onSwitchToLogin}>Log In</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignupModal;
