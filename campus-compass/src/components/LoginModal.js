3cd36f79baa87c9aa158331297763777ed22048b
Dilki Nimeshika Silva <dilkinsilva@gmail.com>
2025-01-17 19:29:56 +0530

    Login and Signup popup windows integration

---
+++ campus-compass/src/components/LoginModal.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Modal.css";
+import { FaFacebookF, FaGoogle } from "react-icons/fa";
+
+
+
+
+const LoginModal = ({ onClose, onSwitchToSignup }) => {
+  return (
+    <div className="modal-overlay">
+      <div className="modal">
+        <button className="close-btn" onClick={onClose}>
+          ×
+        </button>
+        <h2>Log In</h2>
+        <form>
+          
+          <input type="email" placeholder="Enter your email address" required />
+          <input type="password" placeholder="Enter your password" required />
+          <a href="#" className="forgot-password">Forgot Password?</a>
+          <button type="submit" className="btn primary">Log In</button>
+        </form>
+       
+        <p>
+            <span className="popupptext1">Not a member yet?</span>{" "}
+            <a href="#" onClick={onSwitchToSignup}>Create an Account</a>
+        </p>
+        <p className="popupptext2">or</p>
+
+        <hr/>
+       
+        <button className="btn social google">
+              <FaGoogle style={{ marginRight: "10px" }} /> Sign in with Google
+        </button>
+        <button className="btn social facebook">
+              <FaFacebookF style={{ marginRight: "10px" }} />Sign in with Facebook
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginModal;
